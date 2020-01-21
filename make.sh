cda403b7031486ddc5dc4e93572ab5f378fd237b
Aleksey Kladov <aleksey.kladov@gmail.com>
2020-01-21 18:50:40 +0100

    Add Index

--- make.sh
+++ make.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
+bundle exec asciidoctor ./presentations/index.adoc -o ./target/index.html
+
 for d in ./presentations/*/; do
     bundle exec asciidoctor-revealjs \
        -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.7.0 \
