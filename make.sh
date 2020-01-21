42e8c97949f78cf79a04ed228508ec546e316865
Aleksey Kladov <aleksey.kladov@gmail.com>
2020-01-21 18:00:33 +0100

    Minimal boilerplate

--- make.sh
+++ make.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-bundle exec asciidoctor-revealjs \
+for d in ./presentations/*/; do
+    bundle exec asciidoctor-revealjs \
        -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.7.0 \
-       ./presentations/*.adoc  \
-       -D ./target
+       $d/slides.adoc  \
+       -o ./target/$(basename $d).html
+done
