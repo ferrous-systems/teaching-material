f7f45d6dcfa9c13fb7088bc90cbdaf3e40d573ea
Aleksey Kladov <aleksey.kladov@gmail.com>
2020-01-23 17:45:38 +0100

    Attrs in command line

--- make.sh
+++ make.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 set -e
 
-bundle exec asciidoctor ./presentations/index.adoc -o ./target/index.html
+ATTRS="--attribute=customcss=slides.css"
+
+bundle exec asciidoctor $ATTRS ./presentations/index.adoc -o ./target/index.html
+cp ./presentations/slides.css ./target/slides.css
 
 for d in ./presentations/*/; do
     echo $d
-    bundle exec asciidoctor-revealjs \
+    bundle exec asciidoctor-revealjs $ATTRS \
        -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.7.0 \
        $d/slides.adoc  \
        -o ./target/$(basename $d).html
