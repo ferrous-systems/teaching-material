d0bb61b6103c22858de10043d91f3c5077a249f6
Aleksey Kladov <aleksey.kladov@gmail.com>
2020-01-23 17:09:42 +0100

    Some cleanup

--- make.sh
+++ make.sh
@@ -4,6 +4,7 @@ set -e
 bundle exec asciidoctor ./presentations/index.adoc -o ./target/index.html
 
 for d in ./presentations/*/; do
+    echo $d
     bundle exec asciidoctor-revealjs \
        -a revealjsdir=https://cdnjs.cloudflare.com/ajax/libs/reveal.js/3.7.0 \
        $d/slides.adoc  \
