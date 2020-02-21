16c2ca3e58408216797b66446940fc1e464b6968
Aleksey Kladov <aleksey.kladov@gmail.com>
2020-02-21 10:28:22 +0100

    Fix typo

--- presentations/control-flow/12.rs
+++ presentations/control-flow/12.rs
@@ -2,7 +2,7 @@ fn read_file(path: &std) -> Result<String, io::Error> {
      let f = File::open(path)?;
 
      let mut buffer = String::new();
-     f.read_to_end(&mut buffer);
+     f.read_to_end(&mut buffer)?;
 
      Ok(buffer)
 }
