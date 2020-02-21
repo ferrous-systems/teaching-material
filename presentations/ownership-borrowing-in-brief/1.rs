efb3a9bf69c8f51f7a98cd2286766b351c725861
Aleksey Kladov <aleksey.kladov@gmail.com>
2020-02-21 16:10:10 +0100

    fix callouts

--- presentations/ownership-borrowing-in-brief/1.rs
+++ presentations/ownership-borrowing-in-brief/1.rs
@@ -3,13 +3,12 @@ use std::io::Write;
 use std::io;
 
 fn main() -> io::Result<()> {
-    let file_create = File::create("test");
+    let file_create = File::create("test"); // <1>
 
-    let mut file = match file_create {
-        Ok(f) => f,
+    let mut file = match file_create {  // <2>
+        Ok(f) => f, // <3>
         Err(e) => panic!("File create failed: {}", e),
     };
 
     file.write_all(b"Hello World!")
-}
-
+}  // <4>
