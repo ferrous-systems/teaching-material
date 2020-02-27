d1f588499bf0d4937682bc79cefe5a4ce144eb83
Florian Gilcher <florian.gilcher@asquera.de>
2020-02-27 08:58:03 +0000

    Update "accepting changes"

--- assignments/solutions/terrarium/accepting_json.rs
+++ assignments/solutions/terrarium/accepting_json.rs
@@ -1,4 +1,3 @@
-#[macro_use]
 extern crate http_guest;
 
 extern crate serde;
@@ -23,22 +22,32 @@ struct Address {
 use http_guest::{Request, Response};
 
 pub fn user_entrypoint(req: &Request<Vec<u8>>) -> Response<Vec<u8>> {
-    let parsed_person = serde_json::from_slice::<Person>(req.body());
+  match *req.method() {
+    http::method::POST => {
+      let parsed_person = serde_json::from_slice::<Person>(req.body());
 
-    match parsed_person {
-        Ok(p) => {
-          Response::builder()
-            .status(200)
-            .body(serde_json::to_vec(&p))
-            .unwrap()
-        },
-        Err(e) => {
-          Response::builder()
-            .status(422)
-            .body(format!("Error occured: {}", e).into())
-            .unwrap()
+      match parsed_person {
+          Ok(p) => {
+            Response::builder()
+              .status(200)
+              .body(serde_json::to_vec(&p).unwrap())
+              .unwrap()
+          },
+          Err(e) => {
+            Response::builder()
+              .status(422)
+              .body(format!("Error occured: {}", e).into())
+              .unwrap()
+          }
         }
+      }
+    },
+    _ => {
+        Response::builder().status(405)
+          .header("allow", "POST")
+          .body(vec![]).unwrap()
     }
+  }
 }
 
 guest_app!(user_entrypoint);
