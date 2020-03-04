6dcdd3dba05fc8808b4d287e27bc36ec84236eea
Florian Gilcher <florian.gilcher@asquera.de>
2020-03-04 21:57:29 +0100

    Fix drop example

--- presentations/drop-panic-abort/1.rs
+++ presentations/drop-panic-abort/1.rs
@@ -1,9 +1,9 @@
 struct LevelDB {
-    handle: *leveldb_database_t
+    handle: *mut leveldb_database_t
 }
 
 impl Drop for LevelDB {
-    fn drop(&self) {
+    fn drop(&mut self) {
         unsafe { leveldb_close(self.handle) };
     }
 }
