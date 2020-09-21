c348e461c57bc72ab001dd7f27d202a1c4e6ba27
Florian Gilcher <florian.gilcher@asquera.de>
2020-09-21 15:23:25 +0200

    Add non-threaded solution

---
+++ assignments/solutions/mailbox/src/main.rs
@@ -0,0 +1,75 @@
+use std::net::{TcpListener,TcpStream};
+use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::collections::VecDeque;
+use std::sync::{Arc, Mutex};
+
+#[derive(Debug)]
+enum ServerError {
+    ParseError(redisish::Error),
+    IoError(std::io::Error),
+}
+
+impl From<redisish::Error> for ServerError {
+    fn from(e: redisish::Error) -> ServerError {
+        ServerError::ParseError(e)
+    }
+}
+
+impl From<std::io::Error> for ServerError {
+    fn from(e: std::io::Error) -> ServerError {
+        ServerError::IoError(e)
+    }
+}
+
+fn main() -> io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    let mut storage = VecDeque::new();
+
+    for connection in listener.incoming() {
+        let stream = match connection {
+            Ok(stream ) => stream,
+            Err(e) => {
+                println!("Error occured: {:?}", e);
+                continue;
+            }
+        };
+
+
+        let res = handle(stream, &mut storage);
+
+        if let Err(e) = res {
+            println!("Error occured: {:?}", e);
+        }
+    };
+
+    Ok(())
+}
+
+fn handle(mut stream: TcpStream, storage: &mut VecDeque<String>) -> Result<(), ServerError> {
+    let command = read_command(&mut stream)?;
+
+    match command {
+        redisish::Command::Publish(message) => {
+            storage.push_back(message);
+        }
+        redisish::Command::Retrieve => {
+            let data = storage.pop_front();
+            match data {
+                Some(message) => { write!(stream, "{}", message)? },
+                None => { write!(stream, "No message in inbox!\n")? }
+            };
+        }
+    }
+    Ok(())
+}
+
+fn read_command(stream: &mut TcpStream) -> Result<redisish::Command, ServerError> {
+    let mut read_buffer = String::new();
+    let mut buffered_stream = BufReader::new(stream);
+    buffered_stream.read_line(&mut read_buffer)?;
+    let command = redisish::parse(&read_buffer)?;
+    Ok(command)
+}
