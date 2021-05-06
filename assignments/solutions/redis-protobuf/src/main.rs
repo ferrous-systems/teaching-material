f9e9c285122953f6fca3fbe974ac29c4574329e5
Anatol Ulrich <anatol.ulrich@ferrous-systems.com>
2021-05-06 09:57:06 +0200

    const scoping

--- assignments/solutions/redis-protobuf/src/main.rs
+++ assignments/solutions/redis-protobuf/src/main.rs
@@ -6,9 +6,6 @@ use phone::phonebook::{Entry, PhoneBook};
 use redis::IntoConnectionInfo;
 use redis_client::RedisClient;
 
-const KEY_CONFIG: &'static str = "CONFIG";
-const KEY_PHONE_BOOK: &'static str = "PHONE_BOOK";
-
 struct ProtoBufRedisClient {
     redis_client: RedisClient,
 }
@@ -21,8 +18,11 @@ impl ProtoBufRedisClient {
 }
 
 impl ProtoBufRedisClient {
+    const KEY_CONFIG: &'static str = "CONFIG";
+    const KEY_PHONE_BOOK: &'static str = "PHONE_BOOK";
+
     fn get_phone_book(&mut self) -> Result<PhoneBook> {
-        let encoded = self.redis_client.get(KEY_PHONE_BOOK)?;
+        let encoded = self.redis_client.get(Self::KEY_PHONE_BOOK)?;
         let phone_book = PhoneBook::decode(encoded.as_slice())?;
         Ok(phone_book)
     }
@@ -30,12 +30,13 @@ impl ProtoBufRedisClient {
     fn set_phone_book(&mut self, phone_book: &PhoneBook) -> Result<()> {
         let mut buf = Vec::with_capacity(phone_book.encoded_len());
         phone_book.encode(&mut buf)?;
-        self.redis_client.set(KEY_PHONE_BOOK, buf.as_slice())?;
+        self.redis_client
+            .set(Self::KEY_PHONE_BOOK, buf.as_slice())?;
         Ok(())
     }
 
     fn get_config(&mut self) -> Result<Config> {
-        let encoded = self.redis_client.get(KEY_CONFIG)?;
+        let encoded = self.redis_client.get(Self::KEY_CONFIG)?;
         let config = Config::decode(encoded.as_slice())?;
         Ok(config)
     }
@@ -43,7 +44,7 @@ impl ProtoBufRedisClient {
     fn set_config(&mut self, config: &Config) -> Result<()> {
         let mut buf = Vec::with_capacity(config.encoded_len());
         config.encode(&mut buf)?;
-        self.redis_client.set(KEY_CONFIG, buf.as_slice())?;
+        self.redis_client.set(Self::KEY_CONFIG, buf.as_slice())?;
         Ok(())
     }
 }
