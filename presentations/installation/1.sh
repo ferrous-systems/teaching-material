066ad621cf670f39d2b1d06970920cc99bbd0ef6
Lotte Steenbrink <lotte.steenbrink@ferrous-systems.com>
2021-07-13 11:16:01 +0200

    toolchain: clarify commands, rm superfluous ones

--- presentations/installation/1.sh
+++ presentations/installation/1.sh
@@ -1,4 +1,4 @@
-# Installation of a toolchain
+# Installation of a toolchain (here: the stable release channel)
 $ rustup install stable
 
 # Selection of a default toolchain
@@ -7,11 +7,8 @@ $ rustup default stable
 # Override the default toolchain in your directory
 $ rustup override set stable
 
-# Display documentation in browser
-$ rustup doc [--std]
-
 # List supported targets
 $ rustup target list
 
-# Add and install a target to the toolchain
-$ rustup target add <target>
+# Add and install a target to the toolchain (here: to cross-compile for an ARMv6-M target)
+$ rustup target add thumbv6m-none-eabi
