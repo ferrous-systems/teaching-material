2f255d31b1bda237ba5ea832ff4fc615d3de4257
Anatol Ulrich <anatol.ulrich@ferrous-systems.com>
2021-09-23 18:19:41 +0200

    remove outdated info

--- assignments/preliminary/calculator/calc-ffi/solution/lib.rs
+++ assignments/preliminary/calculator/calc-ffi/solution/lib.rs
@@ -3,23 +3,6 @@ use std::ffi::CStr;
 use std::os::raw::c_char;
 use std::panic::catch_unwind;
 
-// What I want to expose:
-// * single function
-// * takes a C string of "input"
-// * returns (if successful) the output
-
-// parse_and_eval
-//
-// Arguments?
-//    * const char
-// Return type?
-//    * integer
-
-// returns 0 if success, returns nonzero if failure
-// on success, output is updated with the result?
-//
-// int parse_and_eval(char*, int64_t* output);
-
 pub use calc::Expr;
 
 #[repr(C)]
