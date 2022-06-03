785e4a68adae132d1792c54e150cf1fbcd4483cf
Amanjeev Sethi <aj@amanjeev.com>
2022-06-03 12:46:18 -0400

    example of char literal allowed one codepoint long only
    Co-authored-by: Андрей Листочкин (Andrei Listochkin) <andrei.listochkin@ferrous-systems.com>

--- presentations/basic-types/6.rs
+++ presentations/basic-types/6.rs
@@ -3,4 +3,5 @@ fn main() {
     let special_char = 'μ';
     let accented_char = 'Ŕ';
     let emoji_char = '☢️';
+    let seven_chars_emoji = '👨‍👩‍👧‍👧'; // Error: Literal must be one character long
 }
