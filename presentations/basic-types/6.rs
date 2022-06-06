45da3a3947b8452eecd79a73a7e8a2be7ba36dea
Jonathan Pallant (Ferrous Systems) <jonathan.pallant@ferrous-systems.com>
2022-06-06 13:28:12 +0100

    The emoji example didn't compile because of a hidden character.
    The editor used to create the file inserted an unprintable extra Unicode Scalar Value - U+FEOF. This is a 'Variation Selector' and it tells the UI to render the previous Code Point as an 'emoji' as opposed to rendering it as 'text'. Unfortunately, this mean the single-quote marks contained two Unicode Scalar Values instead of one, and we consequently got an error from rustc.
    
    This change removes the U+FEOF. The emoji looks rubbish in comparison, but the code does not compile. Or at least, it fails where the comments say it should fail, as opposed to failing earlier on.
    
    See https://unicode-table.com/en/FE0F/

--- presentations/basic-types/6.rs
+++ presentations/basic-types/6.rs
@@ -2,6 +2,6 @@ fn main() {
     let ascii_char = 'r';
     let special_char = 'μ';
     let accented_char = 'Ŕ';
-    let emoji_char = '☢️';
+    let emoji_char = '☢';
     let seven_chars_emoji = '👨‍👩‍👧‍👧'; // Error: char must be one codepoint long
 }
