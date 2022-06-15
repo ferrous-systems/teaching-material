860325d50da48108b3dfc181b63e2beda416775e
Andrei Listochkin <andrei.listochkin@ferrous-systems.com>
2022-06-15 13:23:07 +0100

    Revert "fix: char radioactive emoji"

--- presentations/basic-types/6.rs
+++ presentations/basic-types/6.rs
@@ -6,7 +6,7 @@ fn main() {
     // U+0154 LATIN CAPITAL LETTER R WITH ACUTE
     let accented_char = 'Ŕ';
     // U+2622 RADIOACTIVE SIGN
-    let emoji_char = '☢️';
+    let emoji_char = '\u{2622}';
     // U+1F468 U+200D U+1F469 U+200D U+1F467 U+200D U+1F467
     let seven_chars_emoji = '👨‍👩‍👧‍👧'; // Error: char must be one codepoint long
 }
