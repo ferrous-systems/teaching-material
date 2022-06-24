7adb7c20cca4515069e482253a0856a05acbb8b0
Andrei Listochkin <andrei.listochkin@ferrous-systems.com>
2022-06-24 15:26:25 +0100

    better explanation of Byte string literals

--- presentations/strings/8.rs
+++ presentations/strings/8.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let byte_string = b"allows ASCII and \xF0\x9F\x98\x80 only";
+    let byte_string: &[u8] = b"allows ASCII and \xF0\x9F\x98\x80 only";
     println!("Can't be 'Display'ed {:?}", byte_string);
     if let Ok(string) = std::str::from_utf8(byte_string) {
         println!("Now can Display '{}'", string);
