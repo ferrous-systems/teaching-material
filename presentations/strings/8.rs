f7533cc95637264084a70627173fbd6ce09c283a
Андрей Листочкин (Andrei Listochkin) <andrei.listochkin@ferrous-systems.com>
2022-06-24 18:46:08 +0100

    explain what traits byte strings implement
    Co-authored-by: Jonathan Pallant <jonathan.pallant@ferrous-systems.com>

--- presentations/strings/8.rs
+++ presentations/strings/8.rs
@@ -1,6 +1,6 @@
 fn main() {
     let byte_string: &[u8] = b"allows ASCII and \xF0\x9F\x98\x80 only";
-    println!("Can't be 'Display'ed {:?}", byte_string);
+    println!("Can Debug fmt but not Display fmt: {:?}", byte_string);
     if let Ok(string) = std::str::from_utf8(byte_string) {
         println!("Now can Display '{}'", string);
     }
