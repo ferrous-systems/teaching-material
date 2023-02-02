a6cda38d43e5a6ee7dac9982cac6d5f2dfab7291
Miguel Raz Guzmán Macedo <miguelraz@ciencias.unam.mx>
2023-02-02 08:40:49 -0600

    rename control flow variable example to avoid semantic satiation

--- presentations/control-flow/7.rs
+++ presentations/control-flow/7.rs
@@ -1,8 +1,8 @@
 fn main() {
-    let sum: Option<u8> = 5_u8.checked_add(5);
+    let result: Option<u8> = 5_u8.checked_add(5);
 
-    match sum {
-        Some(sum) if sum % 2 == 0 => println!("5+5 is even!"),
+    match result {
+        Some(result) if result % 2 == 0 => println!("5+5 is even!"),
         _ => println!("5+5 ... isn't even?"),
     }
 }
