a7506e255125b31b46345b1f59c33a05f4a8f5d5
Miguel Raz Guzmán Macedo <miguelraz@ciencias.unam.mx>
2023-02-02 12:17:56 -0600

    feb 2 TMReview work

--- presentations/overview/1.rs
+++ presentations/overview/1.rs
@@ -1,18 +1,10 @@
-use std::io; // <1>
-use std::io::prelude::*;
-use std::fs::File;
+fn main() {
+    let random_number = generate_random_number();
+    let mut my_choice = 10;
+    my_choice += random_number;
+    println!("{my_choice}");
+}
 
-fn main() -> Result<(), io::Error> { // <2>
-    let open_file = File::open("test"); // <3>
-
-    let mut file = match open_file { // <4>
-        Ok(file) => file,
-        Err(e) => return Err(e)
-    };
-
-    let mut buffer = String::new(); // <5>
-    file.read_to_string(&mut buffer)?; // <6>
-    println!("{}", buffer);
-
-    Ok(()) // <7>
+fn generate_random_number() -> i32 {
+    4 // chosen by dice roll, guaranteed to be random
 }
